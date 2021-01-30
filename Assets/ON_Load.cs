21040416b5e30703eb60b03c67490d849ca04332
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2021-01-30 17:46:50 -0500

    got a main menu going woo. now to make set pieces of puzzle platforming

---
+++ Assets/ON_Load.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ON_Load : MonoBehaviour
+{
+	
+	public GameObject menu;
+	public GameObject loadingInterface;
+	public Image progressbar;
+	public TextMeshPro text;
+	
+	List<AsyncOperation> scenesToLoad = new List<AsyncOperation>();
+	
+
+	public void StartGame()
+	{
+		HideMenu();
+		ShowLoadingScreen();
+		scenesToLoad.Add(SceneManager.LoadSceneAsync("SampleScene"));
+		scenesToLoad.Add(SceneManager.LoadSceneAsync("StartingArea", LoadSceneMode.Additive));
+		StartCoroutine(LoadingScrene());
+		
+		
+	}
+	public void HideMenu()
+	{
+		menu.SetActive(false);
+	}
+	public void ShowLoadingScreen()
+	{
+		loadingInterface.SetActive(true);
+	}
+	
+	public IEnumerator LoadingScrene()
+	{
+		float totalProgress =0f;
+		for(int i =0; i<scenesToLoad.Count;++i)
+		{
+			totalProgress += scenesToLoad[i].progress;
+			progressbar.fillAmount = totalProgress/scenesToLoad.Count;
+			yield return null;
+		}
+	}
+	public void ExitGame(){
+		Application.Quit();
+	}
+	
+   
+}
