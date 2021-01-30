350d57e68d7b659bf4d2c5d72339f25af047678b
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2021-01-29 20:37:22 -0500

    Initial commit

---
+++ Assets/Sprites/Enemies/Box/scriptable object/Stats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName= "enemyBox stats",menuName = "stats1", order =1)]
+public class Stats : ScriptableObject
+{
+	public string _name;
+	public float speed;
+	public string drop;
+	public Color color;
+	public int enemyHealth;
+ 
+  
+}
